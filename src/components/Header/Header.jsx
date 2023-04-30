e2141b5b4b606f4a4c00ccfed2f0dc4104277e75
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-04-30 22:16:28 +0300

    processing header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
 import "./Header.css";
+import logo from "../../img/svg/logo.svg";
 
 export function Header() {
   return (
     <>
-      <p>heade</p>
+      <header className="header">
+        <a href="/" className="header-logo">
+          <img className="header-logo-img" src={logo} alt="медичний знак" />
+          <p className="header-logo-title">Лікар-хірург Шабашов А.Д.</p>
+        </a>
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-item">
+              <a className="header-nav-link" href="/">
+                На головну
+              </a>
+            </li>
+            <li className="header-nav-item">
+              <a className="header-nav-link" href="/about">
+                Про лікара
+              </a>
+            </li>
+            <li className="header-nav-item">
+              <a className="header-nav-link" href="/consultation">
+                Записатися на консультацію
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <ul className="header-contacts-list">
+          <li className="header-contacts-item">qweqwe</li>
+          <li className="header-contacts-item">asdasd</li>
+        </ul>
+      </header>
     </>
   );
 }
