54fa4ebea0a5eb22cba159c483cdb6525a3d7304
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-06 23:37:52 +0300

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,21 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <section className="home-section"></section>
+      <section className="home-section">
+        <section className="hero-section">
+          <h1>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            iure perspiciatis ratione nisi debitis voluptatum saepe, accusamus
+            cumque ipsa iste atque alias!
+          </h1>
+        </section>
+        <ul>
+          <li>qwe</li>
+          <li>qwe</li>
+          <li>qwe</li>
+          <li>qwe</li>
+        </ul>
+      </section>
     </>
   );
 };
