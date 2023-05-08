fb7ca7900d72ae5463fdf3c20f574bce061b1d6a
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-09 02:47:44 +0300

    hero gallery add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import "./Home.css";
 import { useState } from "react";
+import rightArrow from "../../img/svg/right-arrow.svg";
+import leftArrow from "../../img/svg/left-arrow.svg";
 
 const Home = () => {
   const [pageCount, setpageCount] = useState(1);
@@ -20,15 +22,35 @@ const Home = () => {
 
   return (
     <>
-      <section className="home-section">
+      <main className="home-section">
         <button type="button" className="home-section-btn-incr" onClick={incr}>
-          +
+          <img src={rightArrow} alt="стрілочка вправо" />
         </button>
         <button type="button" className="home-section-btn-decr" onClick={decr}>
-          -
+          <img src={leftArrow} alt="стрілочка вліво" />
         </button>
 
-        <section className="hero-section">
+        <section className={`hero-section ${pageCount === 1 ? "active" : ""}`}>
+          <h1>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            iure perspiciatis ratione nisi debitis voluptatum saepe, accusamus
+            cumque ipsa iste atque alias!
+          </h1>
+        </section>
+
+        <section
+          className={`hero-section-second ${pageCount === 2 ? "active" : ""}`}
+        >
+          <h1>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            iure perspiciatis ratione nisi debitis voluptatum saepe, accusamus
+            cumque ipsa iste atque alias!
+          </h1>
+        </section>
+
+        <section
+          className={`hero-section-third ${pageCount === 3 ? "active" : ""}`}
+        >
           <h1>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
             iure perspiciatis ratione nisi debitis voluptatum saepe, accusamus
@@ -49,7 +71,7 @@ const Home = () => {
           <li>asd</li>
           <li>asd</li>
         </ul>
-      </section>
+      </main>
     </>
   );
 };
