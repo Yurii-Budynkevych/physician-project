f0fb2cb08797592724a375aaaf65f362c3ef1c6a
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-13 00:41:51 +0300

    predeploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+axios.defaults.baseURL = "https://physician-project-back.onrender.com/api";
 
 export const getTestimonials = () => {
   return axios
