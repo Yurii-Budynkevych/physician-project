d780862759d21c2571df20a7521876174f3f9b15
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2023-05-13 14:09:23 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
